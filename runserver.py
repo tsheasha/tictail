555a5dad4790c31f2c3b7c65511f0f6ee24a60f9
tsheasha <tarek.sheasha@gmail.com>
2014-06-07 00:42:36 +0200

    Initial Commit

--- runserver.py
+++ runserver.py
@@ -1,3 +1,10 @@
 from todo_list.factory import create_app
-app = create_app()
-app.run()
+import os
+
+if 'DATABASE_URL' in os.environ:
+    application = create_app()
+    return application
+else:
+    app = create_app()
+    app.run()
+
